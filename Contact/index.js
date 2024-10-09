1ddfc3704a370c0ff20b5756f820cb48736f9dc1
Btcfixesthis <143770845+Btcfixesthis@users.noreply.github.com>
2024-10-09 21:27:10 +0100

    Add files via upload

---
+++ Contact/index.js
@@ -0,0 +1,14 @@
+document.querySelector("button").style.backgroundColor = "";
+document.querySelector(".btn1 button").style.backgroundColor = "";
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+ 
+  
+
